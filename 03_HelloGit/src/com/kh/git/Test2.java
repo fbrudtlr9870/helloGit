4e88a4133203ec43713960b0f758799b62b1d3c4
fbrudtlr9870 <fbrudtlr9870@naver.com>
2018-06-12 13:26:48 +0900

    test1브랜치 끝

--- 03_HelloGit/src/com/kh/git/Test2.java
+++ 03_HelloGit/src/com/kh/git/Test2.java
@@ -5,6 +5,7 @@ public class Test2 {
 	public static void main(String[] args) {
 		System.out.println("안녕, 깃으로 본 세상");
 		System.out.println("test1브탠치 시작!!");
+		System.out.println("test1브탠치 끝!!");
 	}
 
 }
